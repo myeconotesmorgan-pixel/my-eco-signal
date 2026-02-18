775d418421ec15cb97b5e68fa84db60abe713f37
馬維駿 <maweijun@maweijundeMacBook-Air.local>
2026-02-18 23:16:24 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const WebSocket = require('ws')
+const http = require('http')
+const wss = new WebSocket.Server({ noServer: true })
+const setupWSConnection = require('y-webrtc/bin/utils.js').setupWSConnection
+
+const host = process.env.HOST || '0.0.0.0'
+const port = process.env.PORT || 3000
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.end('Signaling server is running')
+})
+
+server.on('upgrade', (request, socket, head) => {
+  // You may check auth of request here..
+  const handleAuth = ws => {
+    wss.emit('connection', ws, request)
+  }
+  wss.handleUpgrade(request, socket, head, handleAuth)
+})
+
+wss.on('connection', setupWSConnection)
+
+server.listen(port, host, () => {
+  console.log(`running at '${host}' on port ${port}`)
+})
